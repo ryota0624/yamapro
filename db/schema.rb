6b45bf8aeb6764452ad0cd9b2725987e02fc739e
ryotamac <tennis.suta6@gmail.com>
2015-08-17 16:16:19 +0900

    create user seed

--- db/schema.rb
+++ db/schema.rb
@@ -11,8 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 20150817062159) do
+ActiveRecord::Schema.define(version: 20150817062522) do
 
   create_table "essays", force: :cascade do |t|
     t.string   "text"
@@ -21,8 +20,7 @@ ActiveRecord::Schema.define(version: 20150817062159) do
     t.boolean  "pickup_f"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-
-ActiveRecord::Schema.define(version: 20150817062522) do
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "name",                              null: false
