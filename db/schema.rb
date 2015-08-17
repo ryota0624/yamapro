d262129908bc65d208c469e9ca8ba1e56803cb46
ryotamac <tennis.suta6@gmail.com>
2015-08-17 16:28:21 +0900

    つぶやきモデル、テーブル作成

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150817062522) do
+ActiveRecord::Schema.define(version: 20150817072539) do
 
   create_table "essays", force: :cascade do |t|
     t.string   "text"
     t.integer  "user_id"
     t.string   "title"
     t.boolean  "pickup_f"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.integer  "user_id"
+    t.string   "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
