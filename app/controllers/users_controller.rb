89d4f8a167f8be4a623b83490afc2e8da4eb45ce
ryotamac <tennis.suta6@gmail.com>
2015-08-18 11:17:26 +0900

    fixed sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,7 +20,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to root
+      redirect_to root_path
     else
       render action: :new
     end
@@ -35,7 +35,7 @@ class UsersController < ApplicationController
   private
 
     def user_params
-      params.require(:user).permit(:name, :password, :password_confirmation)
+      params.require(:user).permit(:name, :password, :password_confirmation, :gender)
     end
 
 end
