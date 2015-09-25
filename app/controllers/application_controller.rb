ddf0da49470dd9cca2482563bc0bb7ae12244739
ryotamac <tennis.suta6@gmail.com>
2015-09-25 15:53:39 +0900

    fix share

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
 
   protect_from_forgery with: :exception
   helper_method :current_user, :logged_in?
-  before_filter :require_login
+  before_action :require_login, except: [:index]
   include ApplicationHelper
   helper_method :current_account, :logged_in?
 end
