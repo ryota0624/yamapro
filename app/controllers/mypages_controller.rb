b363ba1fa20c8626d54f047c84f6d6a3108cf35a
syooou <shou.428.201bk@gmail.com>
2015-11-13 11:53:17 +0900

    mylist & mypage css以外できた

--- app/controllers/mypages_controller.rb
+++ app/controllers/mypages_controller.rb
@@ -34,11 +34,23 @@ class MypagesController < ApplicationController
   def my_list #記事のお気に入り
     @list = Mylist.where(user_id: current_user.id)
     @essaylist = @list.map {|listItem| listItem.essay }
+
+    @essay_tags = Array.new
+    @essaylist.each_with_index do |essay, i|
+      middle = TagEssay.where(essay_id: essay.id)
+      @essay_tags[i] = middle.map { |tag| Tag.find(tag.tag_id) }
+    end
   end
 
   def my_list_special #記事のお気に入り
     @list = Mylist.where(user_id: current_user.id)
     @essaylist = @list.map {|listItem| listItem.essay }
+
+    @essay_tags = Array.new
+    @essaylist.each_with_index do |essay, i|
+      middle = TagEssay.where(essay_id: essay.id)
+      @essay_tags[i] = middle.map { |tag| Tag.find(tag.tag_id) }
+    end
   end
 
   def get_user_image
