ceb7db5cfb2e0e903339b310d5b55a0a90102979
ryotamac <tennis.suta6@gmail.com>
2015-12-09 00:29:30 +0900

    fix js

---
+++ public/assets/addjs/essay_new-794b5e311cd68a066d9c12469b8a66b81eb3202766fda2ae206f9671e4eaa246.js
@@ -0,0 +1,67 @@
+'use strict';
+
+window.onload = function () {
+  var state = { title: 0, text: 0 };
+  var titleChangeHandle = titleChange(state);
+  var textChangeHandle = textChange(state);
+
+  var btn = document.getElementById('post-btn');
+  var btnStateChangeFunc = toggleBtnState(btn);
+  btnStateChangeFunc();
+
+  var title = document.getElementById('essay_title');
+  title.onkeyup = function (ev) {
+    titleChangeHandle(ev);
+    if (essayState(state)) {
+      btnStateChangeFunc(true);
+    } else {
+      btnStateChangeFunc(false);
+    }
+  };
+
+  var text = document.getElementById('essay_text');
+  text.onkeyup = function (ev) {
+    textChangeHandle(ev);
+    if (essayState(state)) {
+      btnStateChangeFunc(true);
+    } else {
+      btnStateChangeFunc(false);
+    }
+  };
+};
+
+function toggleBtnState(btn) {
+  return function (bool) {
+    console.log('fuck');
+    if (bool) {
+      btn.removeAttribute('disabled');
+    } else {
+      btn.setAttribute('disabled', 'disabled');
+    }
+    return btn;
+  };
+}
+
+function titleChange(state) {
+  return function (ev) {
+    return state.title = ev.target.value.length;
+  };
+}
+
+function textChange(state) {
+  return function (ev) {
+    return state.text = ev.target.value.length;
+  };
+}
+
+function essayState(state) {
+  var title = state.title;
+  var text = state.text;
+
+  if (title > 0 && text > 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
