67b485e3eecc752312a51ca9bff8691fec896f76
ryotamac <tennis.suta6@gmail.com>
2015-12-09 17:35:25 +0900

    user new btnfix

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,8 +1,15 @@
 class TopController < ApplicationController
   def index 
-    @posts = Essay.where(pickup_f: true).limit 4
-    @essays = Essay.where(pickup_f: false).limit 4
-    @recommends = Essay.all.limit 4
+    @recommends = TagEssay.where(tag_id: 1).limit(4).map do |tag|
+      tag.essay
+    end
+
+    essays = TagEssay.where.not(tag_id: 1).map do |tag|
+      tag.essay
+    end
+    @posts = Essay.where(pickup_f: true).slice(0, 3)
+    @essays  = Essay.where(pickup_f: false).slice(0, 4)
+
     post_img = @posts.map {|essay| ImageEssay.where(essay_id: essay.id).first }
     @posts_img = post_img.map {|essay|  
       if essay.nil? then 
@@ -12,8 +19,6 @@ class TopController < ApplicationController
       end
       re
     }
-    # logger.debug @essays.first
-    logger.debug "&&&&&&&&&&&&&&&&&&"
     @pickUps = Essay.where(pickup_f: true)
   end
 end
