908e0166c139a4e80e3df3bf32c35e4ee9550974
ryotamac <tennis.suta6@gmail.com>
2015-12-10 18:00:39 +0900

    make riotpage

--- app/assets/javascripts/addjs/riottags/riotTags.js
+++ app/assets/javascripts/addjs/riottags/riotTags.js
@@ -4,29 +4,31 @@ riot.tag2('test', '<h1>hoge</h1>', '', '', function(opts) {
 riot.tag2('fuck', '<h2>fuck</h2>', '', '', function(opts) {
 });
 
-riot.tag2('essays', '<ul class="collection with-header"> <li class="collection-header"><h4 id="article-header">Articles</h4></li> <li each="{essay in articles}" class="collection-item"> <essay essay="{essay}"></essay> <li class="collection-item" id="article" name="essays/pickup"> more... </ul> <ul class="collection with-header"> <li class="collection-header"><h4 id="post-header">Posts</h4></li> <li each="{essay in posts}" class="collection-item"> <essay essay="{essay}"></essay> <li class="collection-item" id="post" name="essays"> more... </ul> <ul class="collection with-header"> <li class="collection-header"><h4 id="question-header">Questions</h4></li> <li each="{essay in questions}" class="collection-item"> <essay essay="{essay}"></essay> <li class="collection-item" id="question" name="essays/question"> more... </ul>', '', '', function(opts) {
+riot.tag2('essays', '<essaylist name="{&quot;article&quot;}" essays="{articles}" link="{&quot;essays/pickup&quot;}"></essayList> <essaylist name="{&quot;post&quot;}" essays="{posts}" link="{&quot;essays&quot;}"></essayList> <essaylist name="{&quot;question&quot;}" essays="{questions}" link="{&quot;essays/question&quot;}"></essaylist>', '', '', function(opts) {
    this.posts = opts.posts;
    this.articles = opts.articles;
    this.questions = opts.questions;
-   this.on("mount",() => {
-    var postLink = document.getElementById("post");
-    var articleLink = document.getElementById("article");
-    var questionLink = document.getElementById("question");
-    postLink.onclick = jumpPage;
-    articleLink.onclick = jumpPage;
-    questionLink.onclick = jumpPage;
-   })
-   function jumpPage(ev) {
-    document.location.pathname = ev.target.getAttribute("name");
-   }
 }, '{ }');
 
-riot.tag2('essay', '<p onclick="{() => document.location.pathname = &quot;essays/&quot;+essay.id}">{essay.title}</p>', '', '', function(opts) {
-    this.essay = opts.essay;
+riot.tag2('essaylist', '<ul class="collection with-header"> <li class="collection-header"><h4 id="{name+&quot;-header&quot;}">{name}</h4></li> <li each="{essay in essays}" class="collection-item"> <essay essay="{essay}"></essay> <li class="collection-item" id="{name}" name="{link}"> more... </ul>', '', '', function(opts) {
+    this.link = opts.link;
+    this.essays = opts.essays;
+    this.name = opts.name;
+    function jumpPage(ev) {
+      document.location.pathname = ev.target.getAttribute("name");
+    }
     this.on("mount", () => {
-      document.getElementById("essay")
+      var moreLink = document.getElementById(this.name);
+      moreLink.onclick = jumpPage
     })
 }, '{ }');
 
-riot.tag2('myheader', '<nav> <div class="nav-wrapper"> <a href="" class="brand-logo">Yamapro</a> <ul id="nav-mobile" class="right hide-on-med-and-down"> <li> <a href="javascript:void(0)" onclick="{() => {           document.getElementById(&quot;article-header&quot;).scrollIntoView(true);         }}"> feature article </a> </li> <li> <a href="javascript:void(0)" onclick="{() => {           document.getElementById(&quot;post-header&quot;).scrollIntoView(true);         }}">user posts</a> </li> <li> <a href="javascript:void(0)" onclick="{() => {           document.getElementById(&quot;question-header&quot;).scrollIntoView(true);         }}">questions</a> </li> </ul> </div> </nav>', '', '', function(opts) {
+riot.tag2('essay', '<p onclick="{() => document.location.pathname = &quot;essays/&quot;+essay.id}">title: {essay.title}<br>text: {essay.text.split(0, 100)}</p>', '', '', function(opts) {
+    this.essay = opts.essay;
+    this.on("mount", () => {
+      document.getElementById("essay");
+    })
+}, '{ }');
+
+riot.tag2('myheader', '<nav> <div class="nav-wrapper"> <a href="" class="brand-logo">Yamapro</a> <ul id="nav-mobile" class="right hide-on-med-and-down"> <li> <a href="javascript:void(0)" onclick="{() => {           document.getElementById(&quot;article-header&quot;).scrollIntoView(true);         }}"> article </a> </li> <li> <a href="javascript:void(0)" onclick="{() => {           document.getElementById(&quot;post-header&quot;).scrollIntoView(true);         }}">user posts</a> </li> <li> <a href="javascript:void(0)" onclick="{() => {           document.getElementById(&quot;question-header&quot;).scrollIntoView(true);         }}">questions</a> </li> </ul> </div> </nav>', '', '', function(opts) {
 }, '{ }');
--- app/assets/javascripts/addjs/riottags/riotTags.tag
+++ app/assets/javascripts/addjs/riottags/riotTags.tag
@@ -7,48 +7,43 @@
 </fuck>
 
 <essays>
-  <ul class="collection with-header">
-    <li class="collection-header"><h4 id="article-header">Articles</h4></li>
-    <li each={essay in articles} class="collection-item">
-      <essay essay={essay}></essay>
-    <li class="collection-item" id="article" name="essays/pickup"> more...
-  </ul>
-  <ul class="collection with-header">
-    <li class="collection-header"><h4 id="post-header">Posts</h4></li>
-    <li each={essay in posts} class="collection-item">
-      <essay essay={essay}></essay>
-    <li class="collection-item" id="post" name="essays"> more...
-  </ul>
-    <ul class="collection with-header">
-    <li class="collection-header"><h4 id="question-header">Questions</h4></li>
-    <li each={essay in questions} class="collection-item">
-      <essay essay={essay}></essay>
-    <li class="collection-item" id="question" name="essays/question"> more...
-  </ul>
+  <essayList name={"article"} essays={articles} link={"essays/pickup"}></essayList>
+  <essayList name={"post"} essays={posts} link={"essays"}></essayList>
+  <essaylist name={"question"} essays={questions} link={"essays/question"}></essaylist>
   <script>
    this.posts = opts.posts;
    this.articles = opts.articles;
    this.questions = opts.questions;
-   this.on("mount",() => {
-    var postLink = document.getElementById("post");
-    var articleLink = document.getElementById("article");
-    var questionLink = document.getElementById("question");
-    postLink.onclick = jumpPage;
-    articleLink.onclick = jumpPage;
-    questionLink.onclick = jumpPage;
-   })
-   function jumpPage(ev) {
-    document.location.pathname = ev.target.getAttribute("name");
-   }
   </script>
 </essays>
 
+<essaylist>
+  <ul class="collection with-header">
+    <li class="collection-header"><h4 id={name+"-header"}>{name}</h4></li>
+    <li each={essay in essays} class="collection-item">
+      <essay essay={essay}></essay>
+    <li class="collection-item" id={name} name={link}> more...
+  </ul>
+  <script>
+    this.link = opts.link;
+    this.essays = opts.essays;
+    this.name = opts.name;
+    function jumpPage(ev) {
+      document.location.pathname = ev.target.getAttribute("name");
+    }
+    this.on("mount", () => {
+      var moreLink = document.getElementById(this.name);
+      moreLink.onclick = jumpPage
+    })
+  </script>
+</essaylist>
+
 <essay>
-  <p onclick={ () => document.location.pathname = "essays/"+essay.id }>{essay.title}</p>
+  <p onclick={ () => document.location.pathname = "essays/"+essay.id }>title: {essay.title}<br/>text: {essay.text.split(0, 100)}</p>
   <script>
     this.essay = opts.essay;
     this.on("mount", () => {
-      document.getElementById("essay")
+      document.getElementById("essay");
     })
   </script>
 </essay>
@@ -63,7 +58,7 @@
         onclick={ () => {
           document.getElementById("article-header").scrollIntoView(true);
         }}>
-          feature article
+          article
         </a>
       </li>
       <li>
