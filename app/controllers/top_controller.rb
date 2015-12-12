169c136d35a915b22de83bb105ee6f35196750fb
ryotamac <tennis.suta6@gmail.com>
2015-12-12 18:13:16 +0900

    oyahuko

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,16 +1,9 @@
 class TopController < ApplicationController
   def index 
-    @recommends = TagEssay.where(tag_id: 1).limit(4).map do |tag|
-      tag.essay
-    end
-
-    essays = TagEssay.where.not(tag_id: 1).map do |tag|
-      tag.essay
-    end
     @posts = Essay.where
-    .not(question: true).where(pickup_f: true).slice(0, 3)
+    .not(question: true).where(pickup_f: true).limit 3
     @essays  = Essay
-    .where.not(question: true).where(pickup_f: false).slice(0, 4)
+    .where.not(question: true).where(pickup_f: false).limit 4
     post_img = @posts.map {|essay| ImageEssay.where(essay_id: essay.id).first }
     @posts_img = post_img.map {|essay|
       if essay.nil? then
