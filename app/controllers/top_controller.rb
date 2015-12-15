e6feeeba95cf82f4a33d57bf272a198caa070d0a
ryotamac <tennis.suta6@gmail.com>
2015-12-15 23:38:53 +0900

    create sugest

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -13,7 +13,7 @@ class TopController < ApplicationController
       end
       re
     }
-    proc = Proc.new {
+    sugest = Proc.new {
       if logged_in? 
         mylist = Mylist.group(:essay_id).count().to_a.sort {|a, b| -(a[1].to_i <=> b[1].to_i)}
         mylist.map { |essay_id|
@@ -23,8 +23,7 @@ class TopController < ApplicationController
         Essay.where(question: true).limit 4
       end
      }
-     @questions = proc.call
-     logger.debug @questions
+     @questions = sugest_essays current_user
     if params[:type] == "b"
       render :template => 'top/index_boot'
     end
@@ -34,5 +33,33 @@ class TopController < ApplicationController
     @articles = Essay.where(pickup_f: true).where.not(question: true).limit 5
     @posts = Essay.where(pickup_f: false).where.not(question: true).limit 5
     render :layout => false
+  end
+  
+  def sugest_essays(current_user)
+    tag = current_user.tag_users.first
+    userInfo = nil
+    if tag 
+	   userInfo = Usertag.find(tag.id)
+    end
+    father = nil
+    mother = nil
+    if userInfo 
+	    father = Tag.where(name: userInfo.fage).first
+	    mother = Tag.where(name: userInfo.mage).last
+    end
+    fatherEssays = []
+    if father then
+	  fatherEssays = father.tag_essays.map {|tag|
+		  Essay.find_by(id: tag.essay_id)
+	   }.compact
+    end
+    motherEssays = []
+    if mother then
+	  motherEssays = mother.tag_essays.map {|tag|
+		  Essay.find_by(id: tag.essay_id)
+	   }.compact
+    end
+    
+    fatherEssays.concat motherEssays
   end
 end
